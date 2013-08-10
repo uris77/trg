99b086f610128cad2f0dc38962869ea880b5ca4d
Roberto Guerra <uris77@gmail.com>
2013-08-10 10:31:28 -0600

    Error Handling for show added

--- grails-app/controllers/org/trg/IdentificationController.groovy
+++ grails-app/controllers/org/trg/IdentificationController.groovy
@@ -7,16 +7,26 @@ class IdentificationController {
 
     def list() {
         def ids = []
-        sql.eachRow("SELECT * FROM identification_type"){idType ->
-            ids.add("id": idType.id, "name": idType.name)
+        sql.eachRow("SELECT * FROM identification_type"){
+            ids.add(["id": it.id, "name": it.name])
         }
 
         render ids as JSON
     }
 
     def show() {
-        def id = Long.parseLong(params.id)
-        def idType = sql.firstRow("SELECT * FROM identification_type WHERE id = ${id}")
-        render idType as JSON
+        def requestedId = Long.parseLong(params.id)
+        String query = """
+            SELECT * FROM identification_type
+            WHERE id = :id
+        """
+        def idType = sql.firstRow(query, [id: requestedId])
+
+        if(idType){
+            render idType as JSON
+        } else {
+            render(["No Results"] as JSON)
+        }
+
     }
 }
